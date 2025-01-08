f706cee69a28d7a56faf9a0ad861b7a7fb7e0e0f
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 19:12:43 -0400

    Actualizado el nombre de las clase aduana despacho

--- apis/api_datoscarpeta/views/AduanaDespachoView.py
+++ apis/api_datoscarpeta/views/AduanaDespachoView.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from ..models.ModeloAduanaDespacho import ModeloAduanaDespacho
 from ..serializers.AduanaDespachoSerializer import AduanaDespachoSerializer
 
-class ModeloAduanaDespachoView(viewsets.ModelViewSet):
+class AduanaDespachoView(viewsets.ModelViewSet):
     queryset = ModeloAduanaDespacho.objects.all()
     serializer_class = AduanaDespachoSerializer
