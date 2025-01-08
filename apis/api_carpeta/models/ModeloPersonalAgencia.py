1d0d2a3bf449d45ae0d1286f24e396d62e1a60d0
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 17:38:23 -0400

    Creado modelo para el personal de agencia

---
+++ apis/api_carpeta/models/ModeloPersonalAgencia.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils.timezone import now
+
+class ModeloPersonalAgencia(models.Model):
+    nombre_personal = models.CharField(
+        max_length=25, blank=False, null=False
+    )
+    apellido_personal = models.CharField(
+        max_length=25, blank=False, null=False
+    )
+    telefono_personal = models.CharField(
+        max_length=8, blank=True, null=True
+    )
+
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Personal'
+        verbose_name_plural = 'Personal Agencia'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['nombre_personal', 'apellido_personal'],
+                name='unique_nombre_apellido'
+            )
+        ]
+        ordering = ['creado']
+        indexes = [
+            models.Index(fields=['creado'])
+        ]
+
+    def __str__(self):
+        return f"{self.nombre_personal} {self.apellido_personal}"
