069c53729fa8c7d965cc7db16564b68154671260
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 21:17:20 -0400

    Agregado urls de la api_carpeta al archivo de urls principal

--- rest_docDigital/urls.py
+++ rest_docDigital/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-datoscarpeta/', include('apis.api_datoscarpeta.urls')),
+    path('api-carpeta/', include('apis.api_carpeta.urls')),
 ]
