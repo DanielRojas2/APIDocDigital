e8447bc593f76c908009d875cdd4432966289a2e
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 20:50:57 -0400

    Creado serializer para el modelo documento soporte

---
+++ apis/api_carpeta/serializers/DocumentoSoporteSerializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from ..models.ModeloDocumentoSoporte import ModeloDocumentoSoporte
+
+class DocumentoSoporteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModeloDocumentoSoporte
+        fields = [
+            'carpeta_tipoarchivo',
+            'codigo_referencia',
+            'respaldo',
+            'estado_archivo',
+            'estado_respaldo'
+        ]
