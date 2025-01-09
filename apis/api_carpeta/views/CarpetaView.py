b19e29e42b17eca5b5a79b662a734b67de2052ab
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 22:02:17 -0400

    Creado vista para el reporte de carpetas

--- apis/api_carpeta/views/CarpetaView.py
+++ apis/api_carpeta/views/CarpetaView.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from ..models.ModeloCarpeta import ModeloCarpeta
-from ..serializers.CarpetaSerializer import CarpetaSerializer
+from ..serializers.CarpetaSerializer import CarpetaSerializer, ReporteCarpetaSerializer
 
 class CarpetaView(viewsets.ModelViewSet):
     queryset = ModeloCarpeta.objects.all()
     serializer_class = CarpetaSerializer
+
+class ReporteCarpetaView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ReporteCarpetaSerializer
+    queryset = ModeloCarpeta.objects.all()
