8529e05e9abf81dd582ade48095c15555245f550
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 22:01:13 -0400

    Creado serializer para el reporte de las carpetas

--- apis/api_carpeta/serializers/CarpetaSerializer.py
+++ apis/api_carpeta/serializers/CarpetaSerializer.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from ..models.ModeloCarpeta import ModeloCarpeta
+from ...api_datoscarpeta.serializers import (
+    AduanaDespachoSerializer, CanalAperturaSerializer, ClasificacionCarpetaSerializer,
+    ImportadorSerializer, MercaderiaSerializer, ModalidadDespachoSerializer
+)
+from .PersonalCarpetaSerializer import PersonalCarpetaSerializer
 
 class CarpetaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +20,32 @@ class CarpetaSerializer(serializers.ModelSerializer):
             'fecha_apertura',
             'mercaderia'
         ]
+
+class ReporteCarpetaSerializer(serializers.ModelSerializer):
+    canal = CanalAperturaSerializer.CanalAperturaSerializer()
+    modalidad_despacho = ModalidadDespachoSerializer.ModalidadDespachoSerializer()
+    importador = ImportadorSerializer.ImportadorSerializer()
+    aduana_despacho = AduanaDespachoSerializer.AduanaDespachoSerializer()
+    clasificacion_carpeta = ClasificacionCarpetaSerializer.ClasificacionCarpetaSerializer()
+    mercaderia = MercaderiaSerializer.MercaderiaSerializer()
+    personal_asignado = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ModeloCarpeta
+        fields = [
+            'codigo_interno',
+            'nro_dim',
+            'canal',
+            'modalidad_despacho',
+            'importador',
+            'aduana_despacho',
+            'clasificacion_carpeta',
+            'fecha_apertura',
+            'mercaderia',
+            'personal_asignado'
+        ]
+    
+    def get_personal_asignado(self, obj):
+        from ..models.ModeloPersonalCarpeta import ModeloPersonalCarpeta
+        personal = ModeloPersonalCarpeta.objects.filter(carpeta_asignada=obj)
+        return PersonalCarpetaSerializer(personal, many=True).data
