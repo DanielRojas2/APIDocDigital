ce88e300ac0f983d97298f3b497d3f3101870a93
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 22:01:45 -0400

    Modificado campo en fields para obtener el nombre del personal

--- apis/api_carpeta/serializers/PersonalCarpetaSerializer.py
+++ apis/api_carpeta/serializers/PersonalCarpetaSerializer.py
@@ -2,11 +2,14 @@ from rest_framework import serializers
 from ..models.ModeloPersonalCarpeta import ModeloPersonalCarpeta
 
 class PersonalCarpetaSerializer(serializers.ModelSerializer):
+    personal_asignado_nombre = serializers.CharField(
+        source='personal_asignado.nombre_personal'
+    )
     class Meta:
         model = ModeloPersonalCarpeta
         fields = [
             'id',
-            'personal_asignado',
+            'personal_asignado_nombre',
             'carpeta_asignada',
             'rol_asignado'
         ]
