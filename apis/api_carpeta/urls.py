e5f5e31e17eaa2533a545ad4797ef939b7188af4
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-08 21:16:06 -0400

    Registro de las vistas en el router

---
+++ apis/api_carpeta/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views.CarpetaView import CarpetaView
+from .views.DocumentoSoporteView import DocumentoSoporteView
+from .views.PersonalCarpetaView import PersonalCarpetaView
+from .views.TipoArchivoCarpetaView import TipoArchivoCarpetaView
+
+router = DefaultRouter()
+
+router.register(r'carpeta', CarpetaView, basename='carpeta')
+router.register(
+    r'documento-soporte', DocumentoSoporteView, basename='documento_soporte'
+)
+router.register(
+    r'asignar-personal', PersonalCarpetaView, basename='asignar_personal'
+)
+router.register(
+    r'asignar-tipoarchivo', TipoArchivoCarpetaView,
+    basename='asignar_tipoarchivo'
+)
+
+urlpatterns = router.urls
