e88ed8129f5f8478a788344b949f1fa4916d00cc
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-09 22:34:30 -0400

    Creado vistas para el reporte y los detalles de las Carpetas

--- apis/api_carpeta/views/CarpetaView.py
+++ apis/api_carpeta/views/CarpetaView.py
@@ -1,11 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from ..models.ModeloCarpeta import ModeloCarpeta
-from ..serializers.CarpetaSerializer import CarpetaSerializer, ReporteCarpetaSerializer
+from ..serializers.CarpetaSerializer import (
+    CarpetaSerializer, ReporteCarpetaSerializer, CarpetaDetalleSerializer
+)
 
 class CarpetaView(viewsets.ModelViewSet):
     queryset = ModeloCarpeta.objects.all()
     serializer_class = CarpetaSerializer
 
 class ReporteCarpetaView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = ReporteCarpetaSerializer
     queryset = ModeloCarpeta.objects.all()
+    serializer_class = ReporteCarpetaSerializer
+
+class DetalleCarpetaView(viewsets.ReadOnlyModelViewSet):
+    queryset = ModeloCarpeta.objects.prefetch_related(
+        'tipoarchivo_carpeta_set__tipo_archivo',
+        'tipoarchivo_carpeta_set__documento_soporte'
+    )
+    serializer_class = CarpetaDetalleSerializer
