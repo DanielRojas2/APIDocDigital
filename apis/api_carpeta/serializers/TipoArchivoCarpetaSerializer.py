ba2517aaa7465742b5f824ac97f625a24e53b0bc
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-09 22:33:57 -0400

    Serializer para obtener de manera eficiente el tipo de archivo y su documento de respaldo

--- apis/api_carpeta/serializers/TipoArchivoCarpetaSerializer.py
+++ apis/api_carpeta/serializers/TipoArchivoCarpetaSerializer.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from ..models.ModeloTipoArchivoCarpeta import ModeloTipoArchivoCarpeta
+from .DocumentoSoporteSerializer import DocumentoSoporteSerializer
 
 class TipoArchivoCarpetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = ModeloTipoArchivoCarpeta
         fields = ['id', 'carpeta', 'tipo_archivo']
+
+class TipoArchivoCarpetaDetalleSerializer(serializers.ModelSerializer):
+    tipo_archivo = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='tipo_archivo'
+    )
+    documentos_soporte = DocumentoSoporteSerializer(
+        source='documento_soporte',
+        read_only=True
+    )
+
+    class Meta:
+        model = ModeloTipoArchivoCarpeta
+        fields = ['id', 'tipo_archivo', 'documentos_soporte', 'creado']
