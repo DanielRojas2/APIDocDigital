8ff22e21b9a2d23bc61944887d9182ef5760eea4
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-10 12:53:30 -0400

    Prueba de vista protegida con JWT

--- apis/api_datoscarpeta/views/CanalAperturaView.py
+++ apis/api_datoscarpeta/views/CanalAperturaView.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from ..models.ModeloCanalApertura import ModeloCanalApertura
 from ..serializers.CanalAperturaSerializer import CanalAperturaSerializer
 
 class CanalAperturaView(viewsets.ReadOnlyModelViewSet):
     queryset = ModeloCanalApertura.objects.all()
     serializer_class = CanalAperturaSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
