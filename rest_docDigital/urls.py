94fe5b607f61af5a09dd4b4ad27080a157425a37
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-09 22:29:47 -0400

    Agregado ruta para los archivos media

--- rest_docDigital/urls.py
+++ rest_docDigital/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-datoscarpeta/', include('apis.api_datoscarpeta.urls')),
     path('api-carpeta/', include('apis.api_carpeta.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
