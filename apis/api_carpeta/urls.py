8add488cd2304df8268b4965d0afad3e7559a527
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-01-09 22:34:54 -0400

    Registro de los viewsets al router de la api

--- apis/api_carpeta/urls.py
+++ apis/api_carpeta/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views.CarpetaView import CarpetaView, ReporteCarpetaView
+from .views.CarpetaView import (
+    CarpetaView, ReporteCarpetaView, DetalleCarpetaView
+)
 from .views.DocumentoSoporteView import DocumentoSoporteView
 from .views.PersonalCarpetaView import PersonalCarpetaView
 from .views.TipoArchivoCarpetaView import TipoArchivoCarpetaView
@@ -9,15 +11,16 @@ router = DefaultRouter()
 
 router.register(r'carpeta', CarpetaView, basename='carpeta')
 router.register(r'reporte-carpetas', ReporteCarpetaView, basename='reporte-carpetas')
+router.register(r'detalle-carpeta', DetalleCarpetaView, basename='detalle-carpeta')
 router.register(
-    r'documento-soporte', DocumentoSoporteView, basename='documento_soporte'
+    r'documento-soporte', DocumentoSoporteView, basename='documento-soporte'
 )
 router.register(
-    r'asignar-personal', PersonalCarpetaView, basename='asignar_personal'
+    r'asignar-personal', PersonalCarpetaView, basename='asignar-personal'
 )
 router.register(
     r'asignar-tipoarchivo', TipoArchivoCarpetaView,
-    basename='asignar_tipoarchivo'
+    basename='asignar-tipoarchivo'
 )
 
 urlpatterns = router.urls
