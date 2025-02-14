34664ab99e20028811f1ee90254556cb98c06585
DanielRojas2 <danielrojas.alvaro2@gmail.com>
2025-02-14 18:52:39 -0400

    nuevo origen permitido

--- rest_docDigital/settings/base.py
+++ rest_docDigital/settings/base.py
@@ -130,7 +130,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://doc-digital-ps4glo5tm-danielrojas2s-projects.vercel.app/'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
